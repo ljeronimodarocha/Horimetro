64118096f5cb727a4baf54720b64bd5b10f4ff50
Lucas <Lucas@Lucas>
2016-05-17 21:41:38 -0300

    Update persistence.xml

---
+++ src/projetomauricio/DAO/BuscaRelatorio.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetomauricio.DAO;
+
+import Objetos.Operacao;
+
+/**
+ *
+ * @author Lucas
+ */
+public class BuscaRelatorio {
+    public Operacao Relatorio(Operacao operacao){
+        
+        
+        return operacao;
+    }
+    
+}
